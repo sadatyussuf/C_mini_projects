72bd62d6455d73404a9155e4de07a7ce75767761
Sadat Issah <51789921+sadatyussuf@users.noreply.github.com>
2023-02-18 14:06:09 +0000

    program to calculate a square root pf a number

---
+++ excerism_challenges/square_roots.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int square_root(int num)
+{
+    int res = sqrt(num);
+    return res;
+}
+
+int main()
+{
+    square_root(100);
+    return 0;
+}
